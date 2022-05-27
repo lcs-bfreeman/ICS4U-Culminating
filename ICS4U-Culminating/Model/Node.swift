dec4cd098d0fe523a65a7ea1f0ef6ef2b671fa77
Hayden Couch <hcouch@lcs.on.ca>
2022-05-27 08:57:52 -0400

    Setup input file for graph and the base for nodes.

---
+++ ICS4U-Culminating/Model/Node.swift
@@ -0,0 +1,27 @@
+//
+//  Node.swift
+//  ICS4U-Culminating
+//
+//  Created by Hayden Couch on 2022-05-27.
+//
+
+import Foundation
+
+struct Node: Identifiable {
+    let id: Int
+    let paragraphs: [String]
+    let image: String?
+    let edges: [Int]
+    
+}
+
+
+let nodes = [
+
+    Node(id: 1,
+         paragraphs: ["Example"],
+         image: <#T##String?#>,
+         edges: []
+    ),
+
+]

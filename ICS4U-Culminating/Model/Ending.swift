95bb004d913631ebb5cf8ea30326452da22b0ff2
Bradley Kang <bkang@lcs.on.ca>
2022-05-30 12:32:01 -0400

    Added Ending structure as well as data for Node 3

---
+++ ICS4U-Culminating/Model/Ending.swift
@@ -0,0 +1,14 @@
+//
+//  Ending.swift
+//  ICS4U-Culminating
+//
+//  Created by Brad Kang on 2022-05-30.
+//
+
+import Foundation
+
+struct Ending {
+    let nodeID: Int
+    let ending: Bool
+    let death: Bool
+}

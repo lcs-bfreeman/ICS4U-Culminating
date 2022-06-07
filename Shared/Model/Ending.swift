7fc69cfb99cd65e3aa90de6827201120171c8893
Bradley Kang <bkang@lcs.on.ca>
2022-06-06 22:35:42 -0400

    Added functionality for the Graphviz graph

--- Shared/Model/Ending.swift
+++ Shared/Model/Ending.swift
@@ -8,7 +8,18 @@
 import Foundation
 
 struct Ending {
-    let nodeID: Int
-    let ending: Bool
-    let category: String
+    let classification: EndingClassification
+    let description: String
+    var color: String {
+        switch self.classification {
+        case .best:
+            return "#90C8E0"  // Light blue
+        case .good:
+            return "#FFDE5F"  // Pale yellow
+        case .ok:
+            return "#D7883C"  // Orange
+        case .death:
+            return "#AB1800"  // Deep red
+        }
+    }
 }
---
+++ Shared/Model/Enumerations.swift
@@ -0,0 +1,14 @@
+//
+//  Enumerations.swift
+//  ICS4U-Culminating
+//
+//  Created by Brad Kang on 2022-06-06.
+//
+
+import Foundation
+
+enum EndingClassification: String, CaseIterable {
+    case best = "best"
+    case good = "good"
+    case ok = "ok"
+    case death = "death"
